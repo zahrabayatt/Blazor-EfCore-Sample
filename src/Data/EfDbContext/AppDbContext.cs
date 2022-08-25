597c8a5a569f2a5c4f08ea23d5a2316000ac4d4d
Zahra Bayat <zahra.bayat13799@outlook.com>
2022-08-25 23:35:24 +0430

    add data layer to project

---
+++ src/Data/EfDbContext/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Modules;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EfDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString =
+            @"Server=(localdb)\mssqllocaldb;
+            Database=BlazorEfCpreSample;
+            Trusted_Connection=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
